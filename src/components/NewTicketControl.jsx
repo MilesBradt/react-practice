4089269c8d3893fddec0fa14516b5373da08a908
Student <example@example.com>
2019-02-18 13:18:29 -0800

    Initial commit.  Class example.

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -7,12 +7,20 @@ class NewTicketControl extends React.Component {
     this.state = {
       formVisibleOnPage: false
     };
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+
+  handleClick() {
+    this.setState({formVisibleOnPage: true});
+    console.log("Handle click worked, form visible on page is: " + this.state.formVisibleOnPage);
   }
 
   render(){
     return (
       <div>
         <p>This is the NewTicketControl component!</p>
+        <button onClick={this.handleClick}>Click here to change state</button>
       </div>
     );
   }
